33ca07adb473bd6e7c065c148ccad94e6d9323be
Saud Nasir <saudnasir05@gmail.com>
2026-04-23 02:52:29 +0500

    optimized connection caching

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,35 +1,10 @@
-import mongoose from "mongoose";
-
-// Global variable takay connection cache ho jaye
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+import mongoose ,{mongo} from "mongoose";
+export const connectDb=async()=>{
+try {
+    const MONGO_URL=process.env.MONGO_URL
+    const instance=await mongoose.connect(MONGO_URL)
+    console.log(`connected : ${instance.connection.host}`)
+} catch (error) {
+    console.log(`failed due to : ${error}`)
 }
-
-const connectDB = async () => {
-    if (cached.conn) {
-        return cached.conn; // Agar connection pehle se hai, toh wahi wapis karo
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false, // Vercel ke liye zaruri
-        };
-
-        cached.promise = mongoose.connect(process.env.MONGO_URL, opts).then((mongoose) => {
-            return mongoose;
-        });
-    }
-    
-    try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
-    }
-
-    return cached.conn;
-};
-
-export default connectDB;
+}
