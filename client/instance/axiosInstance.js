0bedaa09a7ac485ed4722d2b04cad3de6f33f7a2
Saud Nasir <saudnasir05@gmail.com>
2026-04-23 00:46:21 +0500

    Updated baseURL to Vercel API

--- client/instance/axiosInstance.js
+++ client/instance/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // Tera backend 8000 port par chal raha hai
-    baseURL: 'http://localhost:8000', 
+    // Ab tera backend Vercel par zinda hai! 🚀
+    baseURL: 'https://projectt-1.vercel.app', 
     
     // YEH BOHT ZAROORI HAI 👇
     // Iske bina tera browser cookies (JWT Token) backend ko nahi bhejega, 
